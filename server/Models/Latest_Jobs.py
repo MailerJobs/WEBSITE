ed7d0d50d133a35a116e7a0c8b271291a1d98b06
Vishal <vishalvarun27@gmail.com>
2024-11-21 10:59:10 +0530

    feat: Added Latest_Jobs Model and Resources for backend.

--- server/Models/Latest_Jobs.py
+++ server/Models/Latest_Jobs.py
@@ -23,3 +23,22 @@ class Latest_Jobs:
         cursor.close()
         conn.close()
         return latest_jobs
+    
+    @staticmethod
+    def create_latest_job(job_id, job_title, salary, experience, education, date_posted, job_org, location):
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("INSERT INTO latest_jobs (job_id, job_title, salary, experience, education, date_posted, job_org, location) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)",
+                       (job_id,job_title,salary,experience,education,date_posted,job_org,location))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        
+    @staticmethod
+    def delete_latest_job(job_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM latest_jobs WHERE job_id = %s", (job_id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from Resources.Latest_Jobs_Resources import LatestListResource
+from Resources.Latest_Jobs_Resources import LatestListResource, LatestResource
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ api = Api(app)
 
 
 api.add_resource(LatestListResource, '/latest_jobs')
-
+api.add_resource(LatestResource, '/latest_jobs/<int:job_id>')
 
 
 
